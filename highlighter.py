467c071266561eb7c103c0f91011708c0d2f604b
Jared.Chang <Jared.Chang@vivotek.com>
2018-02-01 16:47:15 +0800

    Move fail and warning to bottom

--- highlighter.py
+++ highlighter.py
@@ -16,22 +16,40 @@ if __name__ == "__main__":
         current_dir = os.path.dirname(os.path.abspath(
             inspect.getfile(inspect.currentframe())))
         subprocess.call([current_dir + "/ansiconx64/ansicon.exe", "-i"])
-
+        
     elif (is_win10):
         kernel32 = ctypes.windll.kernel32
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+    is_fail = False
+    fail_list = []
+    warning_list = []
 
     for line in sys.stdin:
         firstToken = line.split()[0]
 
         if firstToken == 'PASS':
+            is_fail = False
             print '\033[0;32m' + line.rstrip() + '\033[0m'
 
-        elif firstToken == 'FAIL!':
-            print '\033[1;31m' + line.rstrip() + '\033[0m'
-
         elif firstToken == 'QWARN':
-            print '\033[93m' + line.rstrip() + '\033[0m'
+            is_fail = False
+            warning_list.append(line)
+            
+        elif firstToken == 'Totals:':
+            is_fail = False
+
+            for warning in warning_list:                
+                print '\033[93m' + warning.rstrip() + '\033[0m'
+
+            for fail in fail_list:
+                print '\033[1;31m' + fail.rstrip() + '\033[0m'
+
+            print '\033[1m' + line.rstrip() + '\033[0m'
+
+        elif firstToken == 'FAIL!' or is_fail:
+            is_fail = True
+            fail_list.append(line)
 
         else:
-            print line.rstrip()
+            print '\033[1m' + line.rstrip() + '\033[0m'
