df1852f5ea565a8ea3bdc2d906df1ac81e256b2a
Jared <Jared.Chang@vivotek.com>
2018-05-03 12:16:10 +0800

    Fix installation failed and add new feature
    1. installation failed due to regular expresstion escape character
       so remove regular expression
    
    2. add one click to run testing file include at source file
       by add UT/tst_ forcely

--- Install.py
+++ Install.py
@@ -9,7 +9,41 @@ QT_CREATOR_SETTING_EXTERNAL_TOOLS = "[ExternalTools]"
 QT_CREATOR_SETTING_KEYBOARD_SHOURTCUTS = "[KeyboardShortcuts]"
 
 TESTRUNNER_EXTERNAL_TOOLS = "OverrideCategories\\testrunnerx\\size=1\nOverrideCategories\\testrunnerx\\1\\Tool=qmltestrunnerx"
-TESTRUNNER_SHORTCUT = "Tools.External.qmltestrunnerx=Ctrl+`"
+TESTRUNNER_SHORTCUT = "Tools.External.qmltestrunnerx=Ctrl+E"
+
+TESTRUNNER_SCRIPT = '''@setlocal enableextensions enabledelayedexpansion
+@echo off
+
+SET work_dir=%~dp0
+SET all_arg=%1
+
+:lastarg
+    set "last_arg=%1"
+    shift
+    if not "%2"=="" SET all_arg=%all_arg% %1
+    if not "%1"=="" goto lastarg 
+
+if "x%last_arg:tst_=%"=="x%last_arg%" (
+    FOR %%A in ("%last_arg%") do (
+        SET last_arg=%%~dpAUT\tst_%%~nxA
+    )
+)
+
+SET path=%PATH%;{vast2}
+qmltestrunner -import "{vast2}" -plugins "{vast2}" %all_arg% %last_arg% | python %work_dir%/highlighter.py
+pause'''
+
+TESTRUNNER_QT_EXTERNAL_TOOL = '''<?xml version="1.0" encoding="UTF-8"?>
+<externaltool id="qmltestrunnerx">
+    <description></description>
+    <displayname>qmltestrunnerx</displayname>
+    <category></category>
+    <executable output="ignore" error="ignore" modifiesdocument="no">
+        <path>C:/Windows/System32/cmd.exe</path>
+        <arguments>/c start cmd.exe /c qmltestrunnerx -import "{test_folder}" -input %{{CurrentDocument:FilePath}}</arguments>
+        <workingdirectory>{qt_bin}</workingdirectory>
+    </executable>
+</externaltool>'''
 
 def backup_qt_setting_file():
 
@@ -48,31 +82,26 @@ def modify_qt_creator_settings_file():
 
 def modify_external_setting_file(qml_import_test_path, qt_path):
     
+    global TESTRUNNER_QT_EXTERNAL_TOOL
+
     qt_creator_setting_path = os.getenv("APPDATA") + "\\QtProject\\qtcreator\\externaltools"
 
-    f = open(qt_creator_setting_path + "\\qmltestrunnerx.xml", "r")
-    content = f.read()
-    f.close()
-
-    f = open(qt_creator_setting_path + "\\qmltestrunnerx.xml", "w")
-    content = re.sub("test_folder", qml_import_test_path, content)
-    content = re.sub("qt_bin", qt_path + "\\bin", content)
+    f = open(qt_creator_setting_path + "\\qmltestrunnerx.xml", "w+")
+    content = TESTRUNNER_QT_EXTERNAL_TOOL.format(test_folder = qml_import_test_path, qt_bin = qt_path + "\\bin")
     f.write(content)
     f.close()
 
 
 def modify_testrunner_script(qt_path):
+
+    global TESTRUNNER_SCRIPT
     
     bin_path = qt_path + "\\bin"
     vast_registy = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\VIVOTEK, Inc.\VAST")
     install_path = _winreg.QueryValueEx(vast_registy, "INSTALL_PATH")[0] + "\\Client\\Vast2"
 
-    f = open(bin_path + "\\qmltestrunnerX.bat", "r")
-    content = f.read()
-    f.close()
-
-    f = open(bin_path + "\\qmltestrunnerX.bat", "w")
-    f.write(re.sub("vast2", install_path, content))
+    f = open(bin_path + "\\qmltestrunnerX.bat", "w+")
+    f.write(TESTRUNNER_SCRIPT.format(vast2 = install_path))
     f.close()
 
 
@@ -81,7 +110,6 @@ def copy_file_to_qt_path(qt_path):
     bin_path = qt_path + "\\bin"
 
     shutil.copy2(".\\highlighter.py", bin_path)
-    shutil.copy2(".\\qmltestrunnerX.bat", bin_path)
     distutils.dir_util.copy_tree(".\\ansiconx64", bin_path + "\\ansiconx64")
 
 
@@ -91,9 +119,6 @@ def copy_file_to_qt_creator():
 
     if not os.path.exists(qt_creator_setting_path):
         os.makedirs(qt_creator_setting_path)
-    
-    shutil.copy2(".\\qmltestrunnerx.xml", qt_creator_setting_path)
-
 
 def install(qml_import_test_path, qt_path):
 
