d1d3b51bae77fa386df8eeed58bb59f7a8cc582d
Jared <Jared.Chang@vivotek.com>
2018-07-03 12:57:49 +0800

    Add uninstall and deprecate plugin support
    1. add uninstall command -u / --uninstall
    2. vast2 import plugin/test is deprecated
       you should fix that unit test or
       do not use this tool

--- Install.py
+++ Install.py
@@ -8,13 +8,17 @@ import distutils.dir_util
 QT_CREATOR_SETTING_EXTERNAL_TOOLS = "[ExternalTools]"
 QT_CREATOR_SETTING_KEYBOARD_SHOURTCUTS = "[KeyboardShortcuts]"
 
-TESTRUNNER_EXTERNAL_TOOLS = "OverrideCategories\\testrunnerx\\size=1\nOverrideCategories\\testrunnerx\\1\\Tool=qmltestrunnerx"
-TESTRUNNER_SHORTCUT = "Tools.External.qmltestrunnerx=Ctrl+E"
+TESTRUNNER_EXTERNAL_TOOLS = '''OverrideCategories\\testrunnerx\\size=2
+OverrideCategories\\testrunnerx\\1\\Tool=qmltestrunnerx_autoFind
+OverrideCategories\\testrunnerx\\2\\Tool=qmltestrunnerx'''
 
-TESTRUNNER_SCRIPT = '''@setlocal enableextensions enabledelayedexpansion
+TESTRUNNER_SHORTCUT = '''Tools.External.qmltestrunnerx_autoFind=Ctrl+E
+Tools.External.qmltestrunnerx=Ctrl+`
+'''
+
+TESTRUNNER_SCRIPT_FOR_AUTO_FIND = '''@setlocal enableextensions enabledelayedexpansion
 @echo off
 
-SET work_dir=%~dp0
 SET all_arg=%1
 
 :lastarg
@@ -29,24 +33,38 @@ if "x%last_arg:tst_=%"=="x%last_arg%" (
     )
 )
 
-SET path=%PATH%;{vast2}
-qmltestrunner -import "{vast2}" -plugins "{vast2}" %all_arg% %last_arg% | python %work_dir%/highlighter.py
+qmltestrunner %all_arg% %last_arg% | python highlighter.py
 pause'''
+
+TESTRUNNER_SCRIPT = '''@echo off
+qmltestrunner %* | python highlighter.py
+pause'''
+
+TESTRUNNER_AUTO_FIND_QT_EXTERNAL_TOOL = '''<?xml version="1.0" encoding="UTF-8"?>
+<externaltool id="qmltestrunnerx_autoFind">
+    <description></description>
+    <displayname>qmltestrunnerx_autoFind</displayname>
+    <category></category>
+    <executable output="ignore" error="ignore" modifiesdocument="yes">
+        <path>C:/Windows/System32/cmd.exe</path>
+        <arguments>/c start cmd.exe /c {qt_bin}\\qmltestrunnerx_autoFind.bat -input %{{CurrentDocument:FilePath}}</arguments>
+        <workingdirectory>{qt_bin}</workingdirectory>
+    </executable>
+</externaltool>'''
 
 TESTRUNNER_QT_EXTERNAL_TOOL = '''<?xml version="1.0" encoding="UTF-8"?>
 <externaltool id="qmltestrunnerx">
     <description></description>
     <displayname>qmltestrunnerx</displayname>
     <category></category>
-    <executable output="ignore" error="ignore" modifiesdocument="no">
+    <executable output="ignore" error="ignore" modifiesdocument="yes">
         <path>C:/Windows/System32/cmd.exe</path>
-        <arguments>/c start cmd.exe /c qmltestrunnerx -import "{test_folder}" -input %{{CurrentDocument:FilePath}}</arguments>
+        <arguments>/c start cmd.exe /c {qt_bin}\\qmltestrunnerx.bat -input %{{CurrentDocument:FilePath}}</arguments>
         <workingdirectory>{qt_bin}</workingdirectory>
     </executable>
 </externaltool>'''
 
 def backup_qt_setting_file():
-
     qt_creator_setting_file_path = os.getenv("APPDATA") + "\\QtProject\\QtCreator.ini"
     shutil.copy2(qt_creator_setting_file_path, qt_creator_setting_file_path + ".bk")
 
@@ -80,37 +98,41 @@ def modify_qt_creator_settings_file():
     setting_file.close()
 
 
-def modify_external_setting_file(qml_import_test_path, qt_path):
+def create_external_tools(qt_path):
     
     global TESTRUNNER_QT_EXTERNAL_TOOL
+    global TESTRUNNER_AUTO_FIND_QT_EXTERNAL_TOOL
 
     qt_creator_setting_path = os.getenv("APPDATA") + "\\QtProject\\qtcreator\\externaltools"
 
     f = open(qt_creator_setting_path + "\\qmltestrunnerx.xml", "w+")
-    content = TESTRUNNER_QT_EXTERNAL_TOOL.format(test_folder = qml_import_test_path, qt_bin = qt_path + "\\bin")
+    content = TESTRUNNER_QT_EXTERNAL_TOOL.format(qt_bin = qt_path)
+    f.write(content)
+    f.close()
+
+    f = open(qt_creator_setting_path + "\\qmltestrunnerx_autoFind.xml", "w+")
+    content = TESTRUNNER_AUTO_FIND_QT_EXTERNAL_TOOL.format(qt_bin = qt_path)
     f.write(content)
     f.close()
 
 
-def modify_testrunner_script(qt_path):
+def create_testrunner_scripts(qt_path):
 
-    global TESTRUNNER_SCRIPT
-    
-    bin_path = qt_path + "\\bin"
-    vast_registy = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\VIVOTEK, Inc.\VAST")
-    install_path = _winreg.QueryValueEx(vast_registy, "INSTALL_PATH")[0] + "\\Client\\Vast2"
+    global TESTRUNNER_SCRIPT_FOR_AUTO_FIND
 
-    f = open(bin_path + "\\qmltestrunnerX.bat", "w+")
-    f.write(TESTRUNNER_SCRIPT.format(vast2 = install_path))
+    f = open(qt_path + "\\qmltestrunnerX_autoFind.bat", "w+")
+    f.write(TESTRUNNER_SCRIPT_FOR_AUTO_FIND)
+    f.close()
+
+    f = open(qt_path + "\\qmltestrunnerX.bat", "w+")
+    f.write(TESTRUNNER_SCRIPT)
     f.close()
 
 
 def copy_file_to_qt_path(qt_path):
 
-    bin_path = qt_path + "\\bin"
-
-    shutil.copy2(".\\highlighter.py", bin_path)
-    distutils.dir_util.copy_tree(".\\ansiconx64", bin_path + "\\ansiconx64")
+    shutil.copy2(".\\highlighter.py", qt_path)
+    distutils.dir_util.copy_tree(".\\ansiconx64", qt_path + "\\ansiconx64")
 
 
 def copy_file_to_qt_creator():
@@ -120,24 +142,76 @@ def copy_file_to_qt_creator():
     if not os.path.exists(qt_creator_setting_path):
         os.makedirs(qt_creator_setting_path)
 
-def install(qml_import_test_path, qt_path):
+
+def install(qt_path):
 
     backup_qt_setting_file()
     modify_qt_creator_settings_file()
     copy_file_to_qt_path(qt_path)
-    modify_testrunner_script(qt_path)
+    create_testrunner_scripts(qt_path)
     copy_file_to_qt_creator()
-    modify_external_setting_file(qml_import_test_path, qt_path)
+    create_external_tools(qt_path)
+
+
+def remove_qt_creator_settings():
+
+    global TESTRUNNER_EXTERNAL_TOOLS
+    global TESTRUNNER_SHORTCUT
+
+    qt_creator_setting_file_path = os.getenv("APPDATA") + "\\QtProject\\QtCreator.ini"
+    setting_file = open(qt_creator_setting_file_path, "r")
+
+    content = ''
+
+    for line in setting_file.readlines():
+        not_contain_testrunnerx = line.find('testrunnerx') == -1
+        if not_contain_testrunnerx:
+            content += line
+
+    setting_file.close()
+            
+    setting_file = open(qt_creator_setting_file_path, "w")
+    setting_file.write(content)
+    setting_file.close()
+
+
+def remove_script_and_binary(qt_path):
+
+    shutil.rmtree(qt_path + '\\ansiconx64')
+    os.remove(qt_path + '\\qmltestrunnerX_autoFind.bat')
+    os.remove(qt_path + '\\qmltestrunnerX.bat')
+    os.remove(qt_path + '\\highlighter.py')
+
+
+def remove_external_tools():
+
+    qt_creator_setting_path = os.getenv("APPDATA") + "\\QtProject\\qtcreator\\externaltools\\"
+
+    os.remove(qt_creator_setting_path + "\\qmltestrunnerx.xml")
+    os.remove(qt_creator_setting_path + "\\qmltestrunnerx_autoFind.xml")
+
+
+def uninstall(qt_path):
+
+    backup_qt_setting_file()
+    remove_qt_creator_settings()
+    remove_script_and_binary(qt_path)
+    remove_external_tools()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Install QtCreator QmlTestrunnerUtililty')
     
-    parser.add_argument('qml_import_test_path', help='QML/VAST2/Test')
-    parser.add_argument('qt_path', help='qt/msvc2013')
+    parser.add_argument('qt_path', help='qt/msvc2017/bin')
+    parser.add_argument('--uninstall', '-u', action='store_true')
     
     args = parser.parse_args()
-    install(args.qml_import_test_path, args.qt_path)
+
+    if args.uninstall:
+        uninstall(args.qt_path)
+    else:
+        install(args.qt_path)
+
 
 if __name__ == "__main__":
     main()
--- Readme.md
+++ Readme.md
@@ -1,7 +1,16 @@
-Run install.py pass qml test folder and qt msvc2013 folder
+# Install
 
-ex. python install.py D:\Work\VAST2\VAST2\VAST2\Test C:\Qt\Qt5.7.0\5.7\msvc2013
+python install.py [QT_BINARY_FOLDER] (eg. C:\Qt\Qt5.7.0\5.7\msvc2013\bin)
 
-Shortcut Ctrl + ` will enable on qt creator qml test file
+# Uninstall
+
+python install.py -u [QT_BINARY_FOLDER] (eg. C:\Qt\Qt5.7.0\5.7\msvc2013\bin)
+
+# Usage
+
+Shortcut Ctrl + e will enable on qt creator qml test file and source file with specific folder layout: ./UT/tst_[source_file_name].qml
+Shortcut Ctrl + ` will enable on qt creator qml test file: tst_*.qml
+
+# Reference
 
 Use ANSICON to support win7 console colored output
